82a008c1c4483a518a9fb14d6c5358f4de166c9b
DKReyes <dylankreyes@gmail.com>
2022-10-11 01:13:37 -0500

    Created About Page
    Created base foundation of the about mage.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+  get "/about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
