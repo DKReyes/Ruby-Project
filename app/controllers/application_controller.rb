4eaf42a0db2381c75aacc40f7a197aad26bdb34d
DKReyes <dylankreyes@gmail.com>
2022-10-12 19:44:46 -0500

    Added Hierarchical Search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def application
+    @cards = Card.includes(:rarity).all
+    @rarity = Rarity.all
+  end
 end
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,7 +9,8 @@ class CardsController < ApplicationController
 
   def search
     wildcard_search = "%#{params[:keywords]}%"
+    wildcard_search2 = "%#{params[:rarity]}%"
 
-    @cards = Card.where("name LIKE ?", wildcard_search)
+    @cards = Card.where("name LIKE ? AND rarity_id LIKE ?", wildcard_search, wildcard_search2)
   end
 end
